935507f206d19e0ff3f8d8c4a270ddaee896c787
Bruno <38993322+NewarkX@users.noreply.github.com>
2025-04-07 21:49:30 -0300

    setup com tailwind ok

---
+++ front/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-2x1 text-white font-bold'>
+          Tailwind with React + Vite
+      </h1>
+    </>
+  )
+}
+
+export default App
